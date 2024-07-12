ca53a3b69f77b7407b4d6d6464fa7fadc44173c8
Devnol <pgkoutsoumanis@gmail.com>
2024-07-13 01:27:35 +0300

    (feat): added ability to change keymap or reset board without unplugging (feat): added delays between macro keystrokes (refactor): minor fixupping from last year's codebase
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- code/keymaps.py
+++ code/keymaps.py
@@ -3,81 +3,53 @@ from adafruit_hid.keycode import Keycode
 
 #class mode(Enum):
 ##button modes
-NORMAL = 1 # just like on a keyboard, send on press, release on release
-HOLD = 2 # send on press, release on press again
-VIM = 3 #send once on press, send once on release (optionally different keys)
-MACRO = 4 #send a sequence of keys in series
+PRESS = 1 # just like on a keyboard, send on press, release on release
+LOCK = 2 # send on press, release on press again
+DUAL = 3 #send once on press, send once on release (optionally different keys), this is useful for things like vim where you might want to send a command on press and then escape on release
+MACRO = 4 #send a sequence of keys in series, this is useful for things like opening a terminal and running a command
 
 ##define keymaps below:
 #keymaps are a list of dictionaries, each dictionary represents a button
 #each dictionary has a "mode" key and a "press" key
 #the "mode" key is a mode enum (see above)
 #the "press" key is a list of keycodes to send when the button is pressed, depending on the mode
-#the "release" key is a list of keycodes to send when the button is released, only used in VIM mode
-
-default = (
-    	{
-		"mode": NORMAL,
-		"press": [Keycode.A]
-	},
-	{
-		"mode": NORMAL,
-		"press": [Keycode.B]
-	},
-	{
-		"mode": NORMAL,
-		"press": [Keycode.C]
-	},
-	{
-		"mode": NORMAL,
-		"press": [Keycode.D]
-	},
-	{
-		"mode": NORMAL,
-		"press": [Keycode.E]
-	},
-	{
-		"mode": NORMAL,
-		"press": [Keycode.F]
-	},
-	{
-		"mode": NORMAL,
-		"press": [Keycode.G]
-	}
-)
+#the "release" key is a list of keycodes to send when the button is released, only used in DUAL mode
+#the "delay" key is an array of floats representing the delay in seconds between each key in the macro, only used in MACRO mode
 
 map1 = (
 	{
-		"mode":NORMAL,
+		"mode":PRESS,
 		"press": [Keycode.COMMAND, Keycode.CONTROL, Keycode.SHIFT, Keycode.FOUR]
 	},
 	{
 		"mode": MACRO,
-		"press": [[Keycode.ESCAPE], [Keycode.SHIFT, Keycode.SEMICOLON], [Keycode.W], [Keycode.ENTER]]
+		"press": [[Keycode.ESCAPE], [Keycode.SHIFT, Keycode.SEMICOLON], [Keycode.W], [Keycode.ENTER]],
+		"delay": [0.1, 2, 0.1]
 	},
 	{
-		"mode": NORMAL,
+		"mode": PRESS,
 		"press": [Keycode.SHIFT]
 	},
 	{
-		"mode": NORMAL,
+		"mode": PRESS,
 		"press": [Keycode.L]
 	},
 	{
-		"mode": VIM,
+		"mode": DUAL,
 		"press": [Keycode.I],
 		"release": [Keycode.ESCAPE]
 	},
 	{
-		"mode": NORMAL,
+		"mode": PRESS,
 		"press": [Keycode.UP_ARROW]
 	},
 	{
-		"mode": NORMAL,
+		"mode": PRESS,
 		"press": [Keycode.DOWN_ARROW]
 	}
 )
 
 none = ()
 
-keymapslist = [none, map1, map1, default, map1, default]
+# Assign a keymap to each of the 6 buttons (7th button is reserved for storage mount)
+keymapslist = [none, none, none, none, map1, none]
--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import board
+import microcontroller
 from digitalio import DigitalInOut, Direction, Pull
 from time import sleep
 import usb_hid
@@ -9,7 +10,7 @@ from keymaps import *
 
 kbd = Keyboard(usb_hid.devices)
 
-keymap = default
+keymap = none
 
 def init_keymap():
 	global keymap
@@ -36,33 +37,71 @@ print("keymap initialized")
 
 sleep(4) #wait before setting state to false
 button_state = [False] * len(buttons) #populate a list to store the state of each button
+
+Run = True
+
+# menu loop for changing keymaps
+def menu():
+	global Run
+	while not Run:
+		for button in range(len(buttons)): #btn 7 is reserved for storage mount
+			if not buttons[button].value:
+				if button == 6:
+					microcontroller.reset() # hard reset the board
+				keymap = keymapslist[button]
+				print("keymap changed to " + str(keymapslist[button]))
+				Run = True
+				break
+
+		sleep(0.01)
+	main() # return to main loop
+
 # main operation loop
-while True:
-	for button in range(len(buttons)):
-		mode = keymap[button]["mode"] # get mode
-		if not buttons[button].value: # when button pressed
-			if mode == NORMAL: #basically a switch statement
-				kbd.press(*keymap[button]["press"])
-			elif mode == HOLD: # holds down key on first press, releases on second press
-				sleep(0.2) # extra debounce for hold mode
-				if button_state[button] == False:
+def main():
+	global Run 
+	while Run:
+		for button in range(len(buttons)):
+			mode = keymap[button]["mode"] # get mode
+			if not buttons[button].value: # when button pressed
+				#basically a switch statement
+				if mode == PRESS: #basic send button press
 					kbd.press(*keymap[button]["press"])
-					button_state[button] = True
-					break
-				else:
+				elif mode == LOCK: # holds down key on first press, releases on second press
+					sleep(0.2) # extra debounce for hold mode
+					if button_state[button] == False:
+						kbd.press(*keymap[button]["press"])
+						button_state[button] = True
+						break
+					else:
+						kbd.release(*keymap[button]["press"])
+						button_state[button] = False
+						break
+				elif mode == DUAL and button_state[button] == False: #
+					kbd.send(*keymap[button]["press"])
+				elif mode == MACRO and button_state[button] == False:
+					for key in keymap[button]["press"]:
+						kbd.send(*key)
+						sleep(keymap[button]["delay"][keymap[button]["press"].index(key)]) #delay between each key, stored in the delay array
+				button_state[button] = True
+			if button_state[button] and buttons[button].value: #only runs if state is true and button is released
+				if mode == PRESS:
 					kbd.release(*keymap[button]["press"])
-					button_state[button] = False
+				elif mode == DUAL:
+					kbd.send(*keymap[button]["release"])
+				if mode != LOCK: button_state[button] = False
+		# If both buttons 1 and 7 are pressed for longer than 5 seconds, reset the board
+		rstcount = 0
+		if not buttons[0].value and not buttons[6].value:
+			while rstcount < 5:
+				sleep (1)
+				rstcount += 1
+				if buttons[0].value or buttons[6].value:
+					sleep(1)
 					break
-			elif mode == VIM and button_state[button] == False: #only sends vim key once to prevent iiiiiiii
-				kbd.send(*keymap[button]["press"])
-			elif mode == MACRO and button_state[button] == False:
-				for key in keymap[button]["press"]:
-					kbd.send(*key)
-			button_state[button] = True
-		if button_state[button] and buttons[button].value: #only runs if state is true and button is released
-			if mode == NORMAL:
-				kbd.release(*keymap[button]["press"])
-			elif mode == VIM:
-				kbd.send(*keymap[button]["release"])
-			if mode != HOLD: button_state[button] = False
-	sleep(0.01) # debounce
+			if rstcount == 5:
+				Run = False # exit the loop
+		sleep(0.01) # debounce
+	sleep(2) # wait before returning to menu so user can lift their feet
+	menu() # return to menu
+
+main() # never forget to call the main loop		
